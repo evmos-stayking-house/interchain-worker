f9ffccaea67c9b51e2f91a11443052e679cc0388
gloryan0829 <gloryan0829@icloud.com>
2022-08-11 16:56:37 +0900

    denom 계산 수치 string 화

--- src/util/index.spec.ts
+++ src/util/index.spec.ts
@@ -1,6 +1,11 @@
-import { bigNumDividedByNumber } from './index';
+import { bigNumDividedByNumber, bigNumMultipliedByNumber } from './index';
 
 test('큰 수 나누기', () => {
   const a = bigNumDividedByNumber('9999999999999999999999999999999999', 1e18);
+  console.log(a);
+});
+
+test('큰 수 곱하기', () => {
+  const a = bigNumMultipliedByNumber((1e18 * 9999).toString(), 0.8);
   console.log(a);
 });
